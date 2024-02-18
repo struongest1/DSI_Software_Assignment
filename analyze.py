d272767ef951703d0d92cc2c634ea27311f04a0d
Jimmy Truong <jimmy.w.truong@gmail.com>
2024-02-18 18:39:22 -0500

    redefine class and final commit before merging with main

--- analyze.py
+++ analyze.py
@@ -7,51 +7,17 @@ import requests
 
 
 
-class ConfigLoader:
-    def __init__(self, config_file_path):
-        self.config_file_path = config_file_path
-        self.config = None
-    
-    def load_config(self):
-        try:
-            with open(self.config_file_path, 'r') as file:
-                self.config = yaml.safe_load(file)
-            configtable = {}
-            configtable.update(self.config)
-            # print (self.config)
-        except FileNotFoundError:
-            print(f"Error: Config file '{self.config_file_path}' not found.")
-        except yaml.YAMLError as e:
-            print(f"Error: Failed to load config file. {e}")
-            
-config1 = ConfigLoader('secrets.yml')
-configfilefile = config1.load_config()
+logging.basicConfig(
+    handlers=(logging.StreamHandler(), logging.FileHandler('uci_iris.log')), 
+    level=logging.INFO,
+    )
 
 
 
 
-
-# logging.basicConfig(
-#     handlers=(logging.StreamHandler(), logging.FileHandler('uci_iris.log')), 
-#     level=logging.INFO,
-#     )
-# dataset_url = 'https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv'
-# iris_data = pd.read_csv(dataset_url)
-# logging.info(f'Successfully loaded {dataset_url}')
-# iris_data
-
-# with open ('secrets.yml') as f:
-#     secrets = yaml.safe_load(f)
-# #Get user
-# headers = {'Authorization': 'Bearer ' + secrets['key'],
-#            'Accept': 'application/vnd.github+json'}
-# url = 'https://api.github.com/user'
-# r = requests.get(url, headers=headers)
-
-
-
-class yourteamrepo.Analysis.Analysis(analysis_config:str)
-''' Load config into an Analysis object
+# class yourteamrepo.Analysis.Analysis(analysis_config:str)
+class Analysis:
+''' Load config file into an Analysis object
 
 Load system-wide configuration from `configs/system_config.yml`, user configuration from
 `configs/user_config.yml`, and the specified analysis configuration file
@@ -78,9 +44,6 @@ The configuration files should include parameters for:
     * Default save path
 
 '''
-import yaml 
-
-class Analysis:
     def __init__(self, config_file_path):
         self.config_file_path = config_file_path
         self.config = None
@@ -91,14 +54,17 @@ class Analysis:
                 self.config = yaml.safe_load(file)
             configtable = {}
             configtable.update(self.config)
-            # print (self.config)
+            logging.info(f'Successfully loaded {self.config}')
         except FileNotFoundError:
             print(f"Error: Config file '{self.config_file_path}' not found.")
+            logging.error(f"Error: Config file '{self.config_file_path}' not found.")
         except yaml.YAMLError as e:
             print(f"Error: Failed to load config file. {e}")
+            logging.error(f"Error: Failed to load config file. {e}")
+        assert type(self.config_file_path) == str, f"Path, {self.config_file_path} must be a string"
             
-config1 = ConfigLoader('secrets.yml')
-configfilefile = config1.load_config()
+config1 = Analysis('secrets.yml')
+configfile = config1.analysis_obj()
 
 
 
