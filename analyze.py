70dc069ff0d83bbdced9a70e326e39da4ee139b0
Jimmy Truong <jimmy.w.truong@gmail.com>
2024-02-18 22:25:56 -0500

    Add analysis and graph to script

--- analyze.py
+++ analyze.py
@@ -6,45 +6,44 @@ import os
 import requests
 import matplotlib.pyplot as plt
 import requests
+import matplotlib.ticker as tick
 
 
 
 logging.basicConfig(
-    handlers=(logging.StreamHandler(), logging.FileHandler('uci_iris.log')), 
+    handlers=(logging.StreamHandler(), logging.FileHandler('analysis.log')), 
     level=logging.INFO,
     )
 
-
-
 # class yourteamrepo.Analysis.Analysis(analysis_config:str)
 class Analysis:
-''' Load config file into an Analysis object
 
-Load system-wide configuration from `config.yml`, user configuration from
-`configs/user_config.yml`, and the specified analysis configuration file
+    '''Load config file into an Analysis object
 
-Parameters
-----------
-analysis_config : str
-    Path to the analysis/job-specific configuration file
+    Load system-wide configuration from `config.yml`, user configuration from
+    `configs/user_config.yml`, and the specified analysis configuration file
 
-Returns
--------
-analysis_obj : Analysis
-    Analysis object containing consolidated parameters from the configuration files
+    Parameters
+    ----------
+    analysis_config : str
+        Path to the analysis/job-specific configuration file
 
-Notes
------
-The configuration files should include parameters for:
-    * GitHub API token
-    * ntfy.sh topic
-    * Plot color
-    * Plot title
-    * Plot x and y axis titles
-    * Figure size
-    * Default save path
+    Returns
+    -------
+    analysis_obj : Analysis
+        Analysis object containing consolidated parameters from the configuration files
 
-'''
+    Notes
+    -----
+    The configuration files should include parameters for:
+        * GitHub API token
+        * ntfy.sh topic
+        * Plot color
+        * Plot title
+        * Plot x and y axis titles
+        * Figure size
+        * Default save path
+    '''
     def __init__(self, config_file_path):
         self.config_file_path = config_file_path
         self.config = None
@@ -71,11 +70,8 @@ configfile = config1.analysis_obj()
 
 ''' 
 Retrieve data from github using config file
-
 Parameters:
 link to csv file from config file
-
-
 Returns:
 pandas dataframe of csv file from github
 '''
@@ -89,43 +85,32 @@ except Exception as e:
     raise e
 
 
-load_data(configfile['data'])
-
-
-
 #compute_analysis() -> Any
-'''Analyze previously-loaded data.
 
-This function runs an analytical measure of your choice (mean, median, linear regression, etc...)
-and returns the data in a format of your choice.
 
-Parameters
-----------
-None
+'''Analyze previously-loaded data.'''
+# Get a list of the 10 counties with the leaast amount of forest cover
+top10= dataset.sort_values('2021', ascending=True).head(10)
 
-Returns
--------
-analysis_output : Any
 
-'''
 
 
 #send notification for completed analysis
 def notify_done(message: str) -> None:
-''' Notify the user that analysis is complete.
+    ''' Notify the user that analysis is complete.
 
-Send a notification to the user through the ntfy.sh webpush service.
+    Send a notification to the user through the ntfy.sh webpush service.
 
-Parameters
-----------
-message : str
-  Text of the notification to send
+    Parameters
+    ----------
+    message : str
+    Text of the notification to send
 
-Returns
--------
-None
+    Returns
+    -------
+    None
 
-'''
+    '''
     requests.post(
     configfile['ntfy']['url'],
     data=message.encode('utf-8'),
@@ -136,7 +121,7 @@ notify_done('Jimmy Assignment Complete')
 
 
 #Plot data analysis
-plot_data(save_path:Optional[str] = None) -> matplotlib.Figure
+#plot_data(save_path:Optional[str] = None) -> matplotlib.Figure
 ''' Analyze and plot data
 
 Generates a plot, display it to screen, and save it to the path in the parameter `save_path`, or 
@@ -146,10 +131,17 @@ Parameters
 ----------
 save_path : str, optional
     Save path for the generated figure
-
 Returns
 -------
 fig : matplotlib.Figure
-
 '''
 
+bar_fig, bar_ax = plt.subplots()
+bar_ax.barh(top10[configfile['plot_config']['xvar']], top10['2021'], color="red")
+bar_ax.xaxis.set_major_formatter(tick.StrMethodFormatter('{x:,.0f}'))
+bar_ax.set_axisbelow(True)
+bar_ax.grid(alpha=0.3)
+bar_ax.set_title(configfile['plot_config']['title'], fontsize='12')
+bar_ax.set_xlabel(configfile['plot_config']['ylabel'], fontsize=configfile['plot_config']['Font_size'])
+bar_ax.set_ylabel(configfile['plot_config']['xlabel'], fontsize=configfile['plot_config']['Font_size'])
+bar_fig.savefig('forest.png')
